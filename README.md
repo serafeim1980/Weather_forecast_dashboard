435577c43bd0a5edbc3c18bbaf184f6b24122761
Serafim <seraoriginal21@mail.com>
2025-06-28 14:37:15 +0300

    DAY 33: Remove hardcoded API key, use .env file instead

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather forecast API
+a simple app for weather prediction for the next 5 days using python,plotly and streamlit
+# Description 
+the app predicts the weather for the upcoming 1 to 5 days in any country
+-celcius
+-and images for sky conditions(cloud,rain,snow,sun)
+## to run this app
+bash
+git clone https://github.com/your-username/weather-app.git
+cd weather-app
+pip install -r requirements.txt
+streamlit run app.py
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
-
+# Add title, text input, slider, selectbox and subheader
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -9,13 +10,24 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 11 , 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
 
-d, t = get_data(days)
+if place:
+    # Get the temperature/sky data
+    filtered_data = get_data(place,days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+
+    if option == "Temperature":
+        # i took kelvin temperature from api so i had to -273.15 to show the correct temperature in celsius
+        temperatures = [dict["main"]["temp"] -273.15 for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        # Create a temperature plot
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png",
+                  "Snow": "images/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_conditions]
+        print(sky_conditions)
+        st.image(image_paths, width=115)
